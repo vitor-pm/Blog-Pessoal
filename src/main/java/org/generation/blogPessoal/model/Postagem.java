6e89fcf015caf6fdb3dbddf160be398a8b4a7ab5
vitor-pm <vitormelo9913@gmail.com>
2022-02-17 15:26:24 -0300

    Conexao com o banco

---
+++ src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -0,0 +1,66 @@
+package org.generation.blogPessoal.model;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "postagem")
+public class Postagem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    @Size(min = 5, max = 100)
+    private String title;
+
+    @NotNull
+    @Size(min = 10, max = 500)
+    private String text;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date date = new java.sql.Date(System.currentTimeMillis());
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void setTexto(String text) {
+        this.text = text;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+}
