d90c4c1cb2138a01f5f16ed30925ff8cd6826599
Vitor Melo <vitormelo9913@gmail.com>
2022-03-14 18:52:45 -0300

    @vitor-pm [Task: Customizing Swagger]

--- src/main/java/org/generation/blogPessoal/controller/PostController.java
+++ src/main/java/org/generation/blogPessoal/controller/PostController.java
@@ -2,6 +2,8 @@ package org.generation.blogPessoal.controller;
 
 import java.util.List;
 
+import javax.security.auth.message.callback.SecretKeyCallback.Request;
+
 import org.generation.blogPessoal.model.Post;
 import org.generation.blogPessoal.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,40 +19,51 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/posts")
 @CrossOrigin("*")
+@Tag(name = "Controller de Posts", description = "Controller para cadastrar/listar/deletar/atualizar postagens")
 public class PostController {
 
     @Autowired
     private PostRepository repository;
 
+    @Operation(summary = "Buscar todas as postagem")
     @GetMapping
     public ResponseEntity<List<Post>> findAllPosts() {
         return ResponseEntity.ok(repository.findAll());
     }
 
+    @Operation(summary = "Buscando uma postagem pelo ID")
     @GetMapping("/{id}")
-    public ResponseEntity<Post> findByIdPost(@PathVariable Long id) {
+    public ResponseEntity<Post> findByIdPost(@PathVariable Long id, Request a) {
         return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
                 .orElse(ResponseEntity.notFound().build());
+
     }
 
+    @Operation(summary = "Buscando uma postagem pelo Titulo")
     @GetMapping("/title/{title}")
     public ResponseEntity<List<Post>> getByTitle(@PathVariable String title) {
         return ResponseEntity.ok(repository.findAllByTitleContainingIgnoreCase(title));
     }
 
+    @Operation(summary = "Criar uma nova postagem")
     @PostMapping
     public ResponseEntity<Post> postPost(@RequestBody Post post) {
         return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(post));
     }
 
+    @Operation(summary = "Editar uma postagem")
     @PutMapping
     public ResponseEntity<Post> putPost(@RequestBody Post post) {
         return ResponseEntity.status(HttpStatus.OK).body(repository.save(post));
     }
 
+    @Operation(summary = "Deletar uma postagem pelo ID")
     @DeleteMapping("/{id}")
     public void deletePost(@PathVariable Long id) {
         repository.deleteById(id);
--- src/main/java/org/generation/blogPessoal/controller/ThemeController.java
+++ src/main/java/org/generation/blogPessoal/controller/ThemeController.java
@@ -17,40 +17,50 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/theme")
+@Tag(name = "Controller de Temas", description = "Controller para cadastrar/listar/deletar/atualizar temas")
 public class ThemeController {
 
     @Autowired
     private ThemeRepository repository;
 
+    @Operation(summary = "Listar todo os temas")
     @GetMapping
     public ResponseEntity<List<Theme>> getAll() {
         return ResponseEntity.ok(repository.findAll());
     }
 
+    @Operation(summary = "Buscar um tema pelo ID")
     @GetMapping("/{id}")
     public ResponseEntity<Theme> getById(@PathVariable long id) {
         return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    @GetMapping("/nome/{name}")
+    @Operation(summary = "Buscar um tema pelo nome")
+    @GetMapping("/name/{name}")
     public ResponseEntity<List<Theme>> getByName(@PathVariable String name) {
         return ResponseEntity.ok(repository.findAllByDescriptionContainingIgnoreCase(name));
     }
 
+    @Operation(summary = "Criar um novo tema")
     @PostMapping
     public ResponseEntity<Theme> post(@RequestBody Theme theme) {
         return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(theme));
     }
 
+    @Operation(summary = "Editar um tema")
     @PutMapping
     public ResponseEntity<Theme> put(@RequestBody Theme theme) {
         return ResponseEntity.ok(repository.save(theme));
     }
 
+    @Operation(summary = "Deletar um tema pelo Id")
     @DeleteMapping("/{id}")
     public void delete(@PathVariable long id) {
         repository.deleteById(id);
